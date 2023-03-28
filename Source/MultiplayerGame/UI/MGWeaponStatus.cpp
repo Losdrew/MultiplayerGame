03f7447702d10493fe2ed1616175efaed521907e
Artem Volovyi <artem.volovyi@nure.ua>
2023-03-28 21:07:42 +0300

    Add weapon status widget

---
+++ Source/MultiplayerGame/UI/MGWeaponStatus.cpp
@@ -0,0 +1,91 @@
+// Copyright Artem Volovyi. All Rights Reserved.
+
+
+#include "UI/MGWeaponStatus.h"
+
+#include "MGCharacter.h"
+#include "MGEquipmentManagerComponent.h"
+#include "MGRangedWeaponInstance.h"
+
+void UMGWeaponStatus::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	if (APlayerController* PlayerController = GetOwningPlayer())
+	{
+		PlayerController->OnPossessedPawnChanged.AddDynamic(this, &ThisClass::OnPossessedPawnChanged);
+
+		if (APawn* PlayerPawn = PlayerController->GetPawn())
+		{
+			OnPossessedPawnChanged(nullptr, PlayerPawn);
+		}
+	}
+}
+
+void UMGWeaponStatus::OnPossessedPawnChanged(APawn* OldPawn, APawn* NewPawn)
+{
+	// Unbind equipped/unequipped events from old pawn's equipment manager component
+	if (const AMGCharacter* PlayerCharacter = Cast<AMGCharacter>(OldPawn))
+	{
+		if (UMGEquipmentManagerComponent* EquipmentManagerComponent = PlayerCharacter->FindComponentByClass<UMGEquipmentManagerComponent>())
+		{
+			EquipmentManagerComponent->OnEquipped.RemoveDynamic(this, &ThisClass::OnWeaponEquipped);
+			EquipmentManagerComponent->OnUnequipped.RemoveDynamic(this, &ThisClass::OnWeaponUnequipped);
+		}
+	}
+
+	// Bind equipped/unequipped events to new pawn's equipment manager component
+	if (const AMGCharacter* PlayerCharacter = Cast<AMGCharacter>(NewPawn))
+	{
+		if (UMGEquipmentManagerComponent* EquipmentManagerComponent = PlayerCharacter->FindComponentByClass<UMGEquipmentManagerComponent>())
+		{
+			EquipmentManagerComponent->OnEquipped.AddUniqueDynamic(this, &ThisClass::OnWeaponEquipped);
+			EquipmentManagerComponent->OnUnequipped.AddUniqueDynamic(this, &ThisClass::OnWeaponUnequipped);
+		}
+	}
+}
+
+void UMGWeaponStatus::OnWeaponEquipped(UMGEquipmentManagerComponent* EquipmentManagerComponent, UMGEquipmentInstance* NewEquipmentInstance)
+{
+	CurrentWeaponInstance = Cast<UMGRangedWeaponInstance>(NewEquipmentInstance);
+
+	if (CurrentWeaponInstance)
+	{
+		if (!IsVisible())
+		{
+			SetVisibility(ESlateVisibility::SelfHitTestInvisible);
+		}
+
+		WeaponImage->SetBrushFromTexture(CurrentWeaponInstance->Icon);
+		CurrentAmmoNumber->SetText(FText::AsNumber(CurrentWeaponInstance->GetCurrentAmmo()));
+		TotalAmmoNumber->SetText(FText::AsNumber(CurrentWeaponInstance->GetTotalAmmo()));
+
+		CurrentWeaponInstance->OnCurrentAmmoChanged.AddUniqueDynamic(this, &ThisClass::OnCurrentAmmoChanged);
+		CurrentWeaponInstance->OnTotalAmmoChanged.AddUniqueDynamic(this, &ThisClass::OnTotalAmmoChanged);
+	}
+}
+
+void UMGWeaponStatus::OnWeaponUnequipped(UMGEquipmentManagerComponent* EquipmentManagerComponent, UMGEquipmentInstance* NewEquipmentInstance)
+{
+	if (CurrentWeaponInstance)
+	{
+		if (IsVisible())
+		{
+			SetVisibility(ESlateVisibility::Hidden);
+		}
+
+		CurrentWeaponInstance->OnCurrentAmmoChanged.RemoveDynamic(this, &ThisClass::OnCurrentAmmoChanged);
+		CurrentWeaponInstance->OnTotalAmmoChanged.RemoveDynamic(this, &ThisClass::OnTotalAmmoChanged);
+		CurrentWeaponInstance = nullptr;
+	}
+}
+
+void UMGWeaponStatus::OnCurrentAmmoChanged(UMGRangedWeaponInstance* WeaponInstance, int NewValue)
+{
+	CurrentAmmoNumber->SetText(FText::AsNumber(NewValue));
+}
+
+void UMGWeaponStatus::OnTotalAmmoChanged(UMGRangedWeaponInstance* WeaponInstance, int NewValue)
+{
+	TotalAmmoNumber->SetText(FText::AsNumber(NewValue));
+}
---
+++ Source/MultiplayerGame/UI/MGWeaponStatus.h
@@ -0,0 +1,58 @@
+// Copyright Artem Volovyi. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MGEquipmentManagerComponent.h"
+#include "MGRangedWeaponInstance.h"
+#include "Blueprint/UserWidget.h"
+#include "Components/Image.h"
+#include "Components/TextBlock.h"
+#include "MGWeaponStatus.generated.h"
+
+
+/**
+ * 
+ */
+UCLASS()
+class UMGWeaponStatus : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+
+	UFUNCTION()
+	void OnPossessedPawnChanged(APawn* OldPawn, APawn* NewPawn);
+
+protected:
+
+	UFUNCTION(BlueprintCallable)
+	virtual void OnWeaponEquipped(UMGEquipmentManagerComponent* EquipmentManagerComponent, UMGEquipmentInstance* NewEquipmentInstance);
+
+	UFUNCTION(BlueprintCallable)
+	virtual void OnWeaponUnequipped(UMGEquipmentManagerComponent* EquipmentManagerComponent, UMGEquipmentInstance* NewEquipmentInstance);
+
+	UFUNCTION(BlueprintCallable)
+	virtual void OnCurrentAmmoChanged(UMGRangedWeaponInstance* WeaponInstance, int NewValue);
+
+	UFUNCTION(BlueprintCallable)
+	virtual void OnTotalAmmoChanged(UMGRangedWeaponInstance* WeaponInstance, int NewValue);
+
+	virtual void NativeConstruct() override;
+
+public:
+
+	UPROPERTY(meta = (BindWidget))
+	UImage* WeaponImage;
+
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* CurrentAmmoNumber;
+
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* TotalAmmoNumber;
+
+private:
+
+	UPROPERTY()
+	UMGRangedWeaponInstance* CurrentWeaponInstance;
+};
