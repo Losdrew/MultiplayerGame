53e3590a838c0452766cb43b0e87924b9112bd30
Artem Volovyi <artem.volovyi@nure.ua>
2023-07-10 14:38:13 +0300

    Add custom game state class

---
+++ Source/MultiplayerGame/GameModes/MGGameState.h
@@ -0,0 +1,19 @@
+// Copyright Artem Volovyi. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameState.h"
+#include "MGGameState.generated.h"
+
+/**
+ * AMGGameState
+ *
+ *	The base game state class used by this project
+ */
+UCLASS()
+class AMGGameState : public AGameState
+{
+	GENERATED_BODY()
+
+};
